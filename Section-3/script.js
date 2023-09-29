ad1e874ac27832044911190a601f8a46b50d1e0e
anwar khawli <anwarkhawle@gmail.com>
2023-09-29 12:37:52 +0300

    section-4

--- Section-3/script.js
+++ Section-3/script.js
@@ -1,6 +1,7 @@
 let user_inout = prompt("enter a number, please.");
 
-for(let i = 2; i < user_inout; i++){
+for(let i = 2; i < math.sqrt(user_inout); i++){
     if(user_inout % i != 0)
     console.log(i+" ")
 }
+
